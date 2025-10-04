8ac9aeb50976beb59d243ea39f1f6aea3ea8c41c
Jägermeist <phillenth04@outlook.com>
2025-10-04 10:50:04 +0200

    cleaned the code a bit & documented it all

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import logging
 import scrape
 
 
+# Gets the discord token and the discord channel id from .env file
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
+# Setup basic logging config, and save logs to a file
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] [%(levelname)s] %(message)s",
@@ -20,15 +22,22 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+# Suppress the standard logs from the discord bot, and make a log object to write logs with
 logging.getLogger("discord.client").setLevel(logging.WARNING)
 logging.getLogger("discord.gateway").setLevel(logging.WARNING)
 log = logging.getLogger("DiscordBot")
 
-
+# Give the bot basic intents to only send messages
 intents = discord.Intents.default()
 bot = discord.Client(intents=intents)
 
-def get_color(course):
+
+def get_color(course: str):
+    """
+    Returns a color to use for the bot message-embed, depending on what course it is
+    :param course: String with the current course for today
+    :return: Discord-Color object
+    """
     if "pythonprogrammering" in course:
         color = discord.Color.blue()        # blue
     elif "affärsmannaskap" in course:
@@ -44,8 +53,17 @@ def get_color(course):
 
 
 async def daily_schedule_task():
+    """
+    Runs the scraping and bot every day at 6:00 in the morning.
+    Starts by sleeping until 6:00, then scrapes the schedule website,
+    and finally sends a discord message with the information about today's
+    lecture, if there is a lecture for today
+    :return: None
+    """
+    # Wait for the bot to be fully ready before entering the while loop
     await bot.wait_until_ready()
     while not bot.is_closed():
+        # Create a datetime object for the current time, and the target time
         now = datetime.now()
         target_time = now.replace(hour=6, minute=0, second=0, microsecond=0)
 
@@ -53,10 +71,11 @@ async def daily_schedule_task():
         if now >= target_time:
             target_time += timedelta(days=1)
 
+        # Get the time to wait in seconds and sleep for that long
         wait_seconds = (target_time - now).total_seconds()
         await asyncio.sleep(wait_seconds)
 
-        # För test
+        # For quick testing
         # 2025-10-14 branschdag
         # 2025-10-15 digitalundervisning
         # 2025-10-21 affärsmannaskap
@@ -65,33 +84,41 @@ async def daily_schedule_task():
         # 2025-12-04 devops
         # 2025-12-10 affärsmannaskap (fler klasser)
         # today = "2025-12-04"
+
+        # Get the date for today and the scraped information
         today = str(date.today())
         school_info = scrape.get_schoolday_info(today)
 
-        # Send schedule message
+        # Get the channel to send the message in
         channel = bot.get_channel(CHANNEL_ID)
         if not channel:
             log.error(f"Couldn't find discord channel ({channel})")
         elif channel and school_info:
+            # Get the color for the message embed
             color = get_color(school_info["course"].lower())
 
+            # Add the second tags time if there was an associated second tag
             time = f"{school_info['time_1']} - lunch - {school_info['time_2']}" if school_info["time_2"] else school_info["time_1"]
 
-            description = "\u200b\n"
+            # Create the description for the message embed
+            description = "\u200b\n"  # "\u200b" is an invisible character
             description += f"**• 📚 Kurs:** {school_info['course']}\n\n"
             description += f"**• 👨‍🏫 Lärare:** {school_info['teacher']}\n\n" if school_info["teacher"] else ""
             description += f"**• 🏫 Sal:** {school_info['classroom']}\n\n" if school_info["classroom"] else ""
             description += f"**• ⏰ Tid:** {time}\n\n"
             description += f"**• 👥 Klasser:** {school_info['class_group']}" if school_info["class_group"].lower() != "pia25" else ""
 
+            # Create the embed for the discord message
             embed = discord.Embed(
                 title=f"📅 Dagens Schema! ({school_info['weekday']}, {school_info['date']})",
                 description=description,
                 color=color
             )
 
+            # Send schedule message
             await channel.send(embed=embed)
 
+            # Log the information that was sent
             log.info(
                 "Sent message: "
                 f"{school_info['weekday']} ({school_info['date']}) | "
@@ -105,12 +132,19 @@ async def daily_schedule_task():
 
 @bot.event
 async def on_ready():
+    """
+    This function runs when the discord bot is up and running for the first time
+    :return: None
+    """
     log.info("Discord bot is running!")
+    # Create a task for the daily scraping & message sending
     bot.loop.create_task(daily_schedule_task())
 
 
 if __name__ == "__main__":
     log.info("Starting discord bot...")
+
+    # Run the discord bot and log if there is a crash, or the program just exits
     try:
         bot.run(DISCORD_TOKEN)
     except Exception as e:
--- scrape.py
+++ scrape.py
@@ -12,38 +12,43 @@ URL = os.getenv("URL")
 
 
 def get_soup(url=URL):
-    response = requests.get(url)
-
+    # Gets the stored html for the schedule website, so it doesn't send a lot of requests when testing the code
     #with open("index.html", "r") as f:
     #    content = f.read()
-
+    # soup = BeautifulSoup(content, "html.parser")
+    # Gets the code of the schedule website, makes it into a soup object to use for scraping
+    response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    #soup = BeautifulSoup(content, "html.parser")
 
     return soup
 
 
-def get_schoolday_info(today_date):
+def get_schoolday_info(today_date: str):
+    """
+    Gets the information about today's lecture, if there is a lecture today
+    :param today_date: String containing today's date, e.g. 2025-10-04
+    :return: Dictionary with the information about today's class, or empty dict if there's no lecture for today
+    """
     soup = get_soup()
-
     table = soup.find("table", class_="restable")
-
     days = table.find_all("tr", class_="rr clickable2")
 
     school_day_info = {}
     found_today = False
 
     for day in days:
-        info = day.find_all("td")
 
+        # Get all the information about the tag
+        info = day.find_all("td")
+        weekday = info[1].text.strip()
         date = info[2].text.strip()
         time = info[3].text.strip()
-        weekday = info[1].text.strip()
-        teacher = info[5].text.strip()
-        course = info[7].text.strip()
-        classroom = info[6].text.strip()
         class_group = info[4].text.strip()
+        teacher = info[5].text.strip()
+        classroom = info[6].text.strip()
+        course = info[7].text.strip()
 
+        # If the current tag is about today, then store all the information
         if date == today_date:
             school_day_info["date"] = date
             school_day_info["weekday"] = weekday
@@ -54,10 +59,12 @@ def get_schoolday_info(today_date):
             school_day_info["class_group"] = class_group
             found_today = True
 
+        # If the first tag is found and the second one is a new day/lecture, then don't add that time
         elif found_today and weekday:
             school_day_info["time_2"] = ""
             break
 
+        # If the first tag is found, get the second tags time, for the afternoon time
         elif found_today:
             school_day_info["time_2"] = time
             break
